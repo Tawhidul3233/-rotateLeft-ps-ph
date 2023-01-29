eeae322b5eaedb1ecb1ce4b64860178a66ff7920
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2023-01-29 11:03:07 +0600

    all done and added comment

--- RotateLeft.js
+++ RotateLeft.js
@@ -1,11 +1,15 @@
 // rotateLeft Problem solving using JavaScript
 
 const  rotateLeft = (array, RotateIndexNumber)=>{
+     // Create new array for After Rotated array
      const arrayRotated = [];
+
+     // Loop through array beginning RotateIndexNumber and push the new index value arrayRotated array.
      for(i = RotateIndexNumber ; i < array.length; i++){
           arrayRotated.push(array[i])
      }
 
+     // Loop through array beginning at 0 until RotateIndexNumber again push new index value in arrayRotated array.
      for(i = 0; i < RotateIndexNumber; i++){
           arrayRotated.push(array[i])
      }
