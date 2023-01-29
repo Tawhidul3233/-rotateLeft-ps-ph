1105a5c967a9a515f666bd00ef3b6d58f690ecbe
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2023-01-29 10:52:16 +0600

    new array created successfully for loop to RotateIndexNumber

--- RotateLeft.js
+++ RotateLeft.js
@@ -1,5 +1,12 @@
 // rotateLeft Problem solving using JavaScript
 
-const  rotateLeft = ()=>{
+const  rotateLeft = (array, RotateIndexNumber)=>{
+     const arrayRotated = [];
+     for(i = RotateIndexNumber ; i < array.length; i++){
+          arrayRotated.push(array[i])
+     }
 
+     return arrayRotated;
 }
+
+console.log(rotateLeft([1, 2, 3, 4, 5], 4));
